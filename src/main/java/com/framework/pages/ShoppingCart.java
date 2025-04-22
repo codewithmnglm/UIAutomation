46a493c06d68e39e89ab116051c707eede1e9126
Kumar Mangalam <kumar.mangalam@moneyview.in>
2025-04-22 23:21:43 +0530

    Adding Methods and WebElements on Shopping Cart Page.

--- src/main/java/com/framework/pages/ShoppingCart.java
+++ src/main/java/com/framework/pages/ShoppingCart.java
@@ -1,10 +1,14 @@
 package com.framework.pages;
 
 import com.framework.base.BasePage;
+import com.framework.utils.WaitUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 
 public class ShoppingCart extends BasePage {
@@ -21,11 +25,78 @@ public class ShoppingCart extends BasePage {
     @FindBy(xpath = "//div[@class='col-sm-12']//p[text()='Your shopping cart is empty!']")
     private WebElement msg;
 
-    @FindBy(xpath ="//a[contains(text(), 'Continue Shopping')]")
+    @FindBy(xpath = "//a[contains(text(), 'Continue Shopping')]")
     private WebElement continueShopping;
 
     @FindBy(xpath = "//a[contains(text(), 'Checkout')]")
     private WebElement checkout;
+
+    @FindBy(xpath = "//div[@class='table-responsive']/table/tbody/tr")
+    List<WebElement> shoppingCartSize;
+
+    @FindBy(xpath = "//div[@class='alert alert-success alert-dismissible' and contains(text(), 'You have modified your shopping cart')]")
+    private WebElement modifyShoppingCartAlert;
+
+    public String getProductName(int productIndex) {
+
+        String xpath = "//div[@class='table-responsive']/table/tbody/tr[" + productIndex + "]//td[2]/a";
+        WebElement webElement = this.driver.findElement(By.xpath(xpath));
+        return webElement.getText();
+
+    }
+
+    public String getProductModel(int productIndex) {
+
+        String xpath = "//div[@class='table-responsive']/table/tbody/tr[" + productIndex + "]//td[3]";
+        WebElement webElement = this.driver.findElement(By.xpath(xpath));
+        return WaitUtils.waitForVisibility(this.driver, webElement).getText();
+
+
+    }
+
+    public int getQuantityOfProduct(int productIndex) {
+
+        String xpath = "//div[@class='table-responsive']/table/tbody/tr[" + productIndex + "]//td[4]//input";
+        WebElement webElement = this.driver.findElement(By.xpath(xpath));
+        return Integer.parseInt(WaitUtils.waitForVisibility(this.driver, webElement).getAttribute("value"));
+    }
+
+    public void updateQuantityOfProduct(int productIndex, String quantity) {
+
+        String xpath = "//div[@class='table-responsive']/table/tbody/tr[" + productIndex + "]//td[4]//input";
+        WebElement webElement = this.driver.findElement(By.xpath(xpath));
+        type(WaitUtils.waitForClickable(this.driver, webElement), quantity);
+
+    }
+
+    public void refreshProductQuantity(int productIndex) {
+
+        String xpath = "//div[@class='table-responsive']/table/tbody/tr[" + productIndex + "]//td[4]//button[1][contains(@class, 'btn-primary') and @data-original-title='Update']";
+        WebElement webElement = this.driver.findElement(By.xpath(xpath));
+        click(webElement);
+    }
+
+    public void removeFromCart(int productIndex) {
+
+        String xpath = "//div[@class='table-responsive']/table/tbody/tr[" + productIndex + "]//td[4]//button[2][contains(@class, 'btn btn-danger') and @data-original-title='Remove']";
+        WebElement webElement = this.driver.findElement(By.xpath(xpath));
+        click(webElement);
+    }
+
+
+    public String getProductPrice(int productIndex) {
+
+        String xpath = "//div[@class='table-responsive']/table/tbody/tr[" + productIndex + "]//td[5]";
+        WebElement webElement = this.driver.findElement(By.xpath(xpath));
+        return WaitUtils.waitForVisibility(this.driver, webElement).getText();
+    }
+
+    public String getTotalProductPrice(int productIndex) {
+
+        String xpath = "//div[@class='table-responsive']/table/tbody/tr[" + productIndex + "]//td[6]";
+        WebElement webElement = this.driver.findElement(By.xpath(xpath));
+        return WaitUtils.waitForVisibility(this.driver, webElement).getText();
+    }
 
 
 }
